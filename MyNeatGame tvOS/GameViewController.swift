8cc593a396f241e2e83485c50af3ae6eac1fdd76
Josh Spicer <josh.spicer@microsoft.com>
2024-12-05 10:49:41 -0800

    Initial Commit

---
+++ MyNeatGame tvOS/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  MyNeatGame tvOS
+//
+//  Created by jospicer on 12/5/24.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
